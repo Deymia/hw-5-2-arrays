68ccec4e21bb482911558a5b735c20ef58a62870
Anastasia <shvayka92@mail.ru>
2022-06-21 20:14:09 +0300

    done [2]

--- src/Main.java
+++ src/Main.java
@@ -14,36 +14,39 @@ public class Main {
             arr[i] = random.nextInt(100_000) + 100_000;
         }
 // первое задание
-        float sum = 0;
+        int sum = 0;
         for (int i = 0; i < arr.length; i++) {
             sum +=arr[i];
         }
         System.out.println("Сумма трат за месяц: " + sum);
 // второе задание
-        int min = 0;
+        int min = arr[0];
         int max = 0;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] > max) {
-                max = arr [i];
+                max = arr[i];
+            }
+
+            if (arr[i] < min) {
+                min = arr[i];
             }
         }
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < max) {
-                min = arr [i];
-            }
-        }
+
         System.out.println("Максимальная сумма: " + max);
         System.out.println("Минимальная сумма: " + min);
 // третье задание
         float average = 0.0f;
-        average = sum / arr.length;
+        float sum2 = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum2 +=arr[i];
+        }
+        average = sum2 / arr.length;
         System.out.println("Среднее: " + average);
 
 //четвертое задание
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length-1; i >= 0; i-- )
             System.out.print(reverseFullName[i]);
-
 
         return arr;
     }
